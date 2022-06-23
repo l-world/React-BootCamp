f5df4ac7f5103ccfb00828793e157e798b004956
Echo <39179853+l-world@users.noreply.github.com>
2022-06-23 22:03:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import Navbar from './components/MemeGenerator/Navbar/navbar'
-import Main from './components/MemeGenerator/Main/main'
-import Counter  from './components/Counter/counter';
-import Boxs from "./components/Boxes/Index"
-
+import Signin from './components/Signin/Login'
 export default function App() {    
     return (
         <div className='container'>
-            <Navbar />
-            <Main />
-            <Counter />
-            <Boxs />
+            <Signin />
         </div>
     )
 }
