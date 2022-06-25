4d51c4ce83d4fd18cb7583333610894abcc9d217
echo <heart0magic@163.com>
2022-06-25 19:32:08 +0800

    Implement interactive functions,add, delete, edit notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NoteApp from './pages/NoteApp/NoteApp'
 
 export default function App() {
   return (
     <div className='app'>
-      
+      <NoteApp />
     </div>
   )
 }
