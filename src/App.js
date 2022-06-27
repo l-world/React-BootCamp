485ee3317cf5522b23ce9a4292391eae5b894581
echo <heart0magic@163.com>
2022-06-27 10:05:54 +0800

    Formatting the development environment - what I want

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactFactsMode from './pages/ReactFactMode/Mode'
 
 export default function App() {
   return (
     <div className='app'>
-        <ReactFactsMode />
+        
     </div>
   )
 }
