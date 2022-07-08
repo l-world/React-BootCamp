e0e05d2eba69a7c56243b962fe8486d68dacf76a
echo <heart0magic@163.com>
2022-07-08 22:36:39 +0800

    fix router bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './NoteApp/components/Navbar'
 import Home from './NoteApp/pages/Home'
 import Notes from './NoteApp/pages/Notes'
@@ -9,15 +9,19 @@ import Signup from './NoteApp/pages/SignUp'
 
 export default function App() {
     return (
-        <div className='container'>
-            <Navbar />
+        <>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/notes" element={<Notes />}></Route>
-                <Route path="/contact" element={<Contact />}></Route>
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/signup" element={<Signup />}></Route>
             </Routes>
-        </div>
+            <div className='container'>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/notes" element={<Notes />}></Route>
+                    <Route path="/contact" element={<Contact />}></Route>
+                </Routes>
+            </div>
+        </>
     )
 }
