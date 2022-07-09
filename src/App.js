d649690cec1d538ebcac6c7184987af25cb74f0b
echo <heart0magic@163.com>
2022-07-09 22:00:44 +0800

    add notfound page

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import Notes from './NoteApp/pages/Notes'
 import Contact from './NoteApp/pages/Contact'
 import Login from './NoteApp/pages/Login'
 import Signup from './NoteApp/pages/SignUp'
+import NotFound from './NoteApp/pages/NotFound'
 
 export default function App() {
     return (
         <>
-            <Routes>
+           {/*  <Routes>
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/signup" element={<Signup />}></Route>
-            </Routes>
+                <Route path="*" element={<NotFound />}></Route>
+            </Routes> */}
             <div className='container'>
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/notes" element={<Notes />}></Route>
                     <Route path="/contact" element={<Contact />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                    {/* <Route path="/*" element={<NotFound />}></Route> */}
                 </Routes>
             </div>
+
         </>
     )
 }
--- src/NoteApp/pages/Login.js
+++ src/NoteApp/pages/Login.js
@@ -4,12 +4,15 @@ import loginBg from '../icon/login.svg'
 
 export default function Login() {
     return (
-        <Account
-            bgSrc={loginBg}
-            title="Account Login"
-            btnText="Login"
-            linkText="Sign up here"
-            path="/signup"
-        />
+        <div className="container--login">
+            <Account
+                bgSrc={loginBg}
+                title="Account Login"
+                btnText="Login"
+                linkText="Sign up here"
+                path="/signup"
+            />
+        </div>
+        
     )
 }
